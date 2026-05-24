9713cbf2bbeb65c467580be39117598082d55087
Sergey Brovko <serbro7@gmail.com>
2026-05-24 14:51:12 +0200

    FEAT: implemented chat controller, service and repository. application.properties updated, MongoDB finally implemented

--- src/main/java/com/bolsadeideas/springboot/backend/chat/controllers/ChatController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/chat/controllers/ChatController.java
@@ -3,17 +3,22 @@ package com.bolsadeideas.springboot.backend.chat.controllers;
 import java.util.Date;
 import java.util.Random;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import com.bolsadeideas.springboot.backend.chat.models.documents.Mensaje;
+import com.bolsadeideas.springboot.backend.chat.models.service.ChatService;
 
 //We use "Controller" instead of "RestController" because we are not going to return JSON, but we are going to return a view (HTML)
 @Controller
 public class ChatController {
 
     private String[] colores = {"red", "green", "blue", "magenta", "purple", "orange"};
+
+    @Autowired
+    private ChatService chatService;
 
     //Our message broker has a prefix "/chat/", so we need to send the message to that 
     // prefix, and we need to use the @MessageMapping annotation to map the message to the 
@@ -27,6 +32,8 @@ public class ChatController {
         if(mensaje.getTipo().equals("NUEVO_USUARIO")) {
             mensaje.setColor(colores[new Random().nextInt(colores.length)]);
             mensaje.setTexto("nuevo usuario");
+        } else {
+            chatService.guardar(mensaje);
         }
 
         return mensaje;
